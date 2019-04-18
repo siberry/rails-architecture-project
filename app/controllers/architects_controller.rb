e7109172941541907c9dae76a1cc71592e9304e6
siberry <siberry226@gmail.com>
2019-04-18 12:29:32 -0400

    styling

--- app/controllers/architects_controller.rb
+++ app/controllers/architects_controller.rb
@@ -1,9 +1,9 @@
 class ArchitectsController < ApplicationController
   def show
-
+    @architect = Architect.find(params[:id])
   end
 
   def index
-    @architects = Architect.pages
+    @architects = Architect.all
   end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.pages
+    @locations = Location.all
   end
 
   def show
